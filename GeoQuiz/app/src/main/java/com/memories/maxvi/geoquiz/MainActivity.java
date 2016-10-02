b30c21bddce9f8d16551ec0477702999b3a3c990
DESKTOP-BHIJRK2\maxvi <maxmaxwow@gmail.com>
2016-10-02 16:56:29 +0300

    game logic is ready (have to add some ui features)

--- GeoQuiz/app/src/main/java/com/memories/maxvi/geoquiz/MainActivity.java
+++ GeoQuiz/app/src/main/java/com/memories/maxvi/geoquiz/MainActivity.java
@@ -4,24 +4,57 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_amazon, true),
+            new Question(R.string.question_baikal, true),
+            new Question(R.string.question_batya, true),
+            new Question(R.string.question_london, true),
+            new Question(R.string.question_ocean, false),
+            new Question(R.string.question_minsk, true),
+            new Question(R.string.question_obama, false),
+    };
+
+    private int mCurrentIndex = 0;
+
+    private void updateQuestion(){
+        mQuestionTextView.setText(mQuestionBank[mCurrentIndex].getTextResId());
+    }
+
+    private void checkAnswer(boolean userAnswer){
+        if (userAnswer == mQuestionBank[mCurrentIndex].isAnswerTrue()){
+            Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+        }
+        mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+        updateQuestion();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        updateQuestion();
+
         mTrueButton = (Button) findViewById(R.id.btn_true);
 
         mTrueButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
 
@@ -31,7 +64,17 @@ public class MainActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
+            }
+        });
+
+        mNextButton = (Button) findViewById(R.id.btn_next);
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
             }
         });
     }
